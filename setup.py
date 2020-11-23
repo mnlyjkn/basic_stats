64d98735b4ff70077688b9ff1bcedf18e1da99f6
mnlyjkn <drew073091@aol.com>
2020-11-23 14:06:25 -0500

    another try

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = "Andrew_Million_Basic_stats_module",
     version = "0.1",
-    author = "Brice Loose",
+    author = "Andrew Million",
     packages=['Andrew_Million_Basic_stats_module'],
     install_requires=['numpy','scipy.io'],
 )
